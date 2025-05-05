56f20dd4e92bfb15a25180f9ef6f6694641c69a5
Michael Litvin <michaellitvin1551@gmail.com>
2025-05-05 14:09:01 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace topaz
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Topaz.SigPlusNET sigPlusNET1;
+        private System.Windows.Forms.Panel signaturePanel;
+        private System.Windows.Forms.Button doneButton;
+        private System.Windows.Forms.Button clearButton;
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.signaturePanel = new System.Windows.Forms.Panel();
+            this.sigPlusNET1 = new Topaz.SigPlusNET();
+            this.doneButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // 
+            // signaturePanel
+            // 
+            this.signaturePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.signaturePanel.Location = new System.Drawing.Point(10, 12);
+            this.signaturePanel.BackColor = Color.White;
+            this.signaturePanel.Name = "signaturePanel";
+            this.signaturePanel.Size = new System.Drawing.Size(320, 114);
+            
+            // 
+            // Clear Button
+            //
+            this.clearButton.Location = new System.Drawing.Point(344, 11);
+            this.clearButton.Name = "cmdStart";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 0;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+
+            // 
+            // Done Button
+            //
+            this.doneButton.Location = new System.Drawing.Point(344, 103);
+            this.doneButton.Name = "cmdStart";
+            this.doneButton.Size = new System.Drawing.Size(75, 23);
+            this.doneButton.TabIndex = 0;
+            this.doneButton.Text = "Done";
+            this.doneButton.UseVisualStyleBackColor = true;
+            this.doneButton.Click += new System.EventHandler(this.doneButton_Click);
+
+            // 
+            // sigPlusNET1
+            // 
+            this.sigPlusNET1.BackColor = System.Drawing.Color.White;
+            this.sigPlusNET1.ForeColor = System.Drawing.Color.Black;
+            this.sigPlusNET1.Location = new System.Drawing.Point(20, 0);
+            this.sigPlusNET1.Name = "sigPlusNET1";
+            this.sigPlusNET1.Size = new System.Drawing.Size(320, 114);
+            this.sigPlusNET1.TabIndex = 0;
+            this.sigPlusNET1.Text = "sigPlusNET1";
+            this.sigPlusNET1.PenUp += new System.EventHandler(this.sigPlusNET1_PenUp);
+            this.signaturePanel.Controls.Add(this.sigPlusNET1);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 138);
+            this.Controls.Add(this.signaturePanel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.doneButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Signature Capture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);  // Add the Load event
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System.Drawing.Imaging;
+
+namespace topaz
+{
+    public partial class Form1 : Form
+    {
+        readonly Bitmap sign = new(Application.StartupPath + "imgs\\Sign.bmp");
+        readonly Bitmap done = new(Application.StartupPath + "imgs\\Done.bmp");
+        readonly Bitmap clear = new(Application.StartupPath + "imgs\\Clear.bmp");
+        readonly Bitmap please = new(Application.StartupPath + "imgs\\Please.bmp");
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Call the method that was previously triggered by the button.
+            cmdStart_Click(sender, e);
+        }
+
+        private void cmdStart_Click(object sender, EventArgs e)
+        {
+            //The following code will write BMP images out to the LCD 1X5 screen
+            sigPlusNET1.SetTabletState(1); //Turns tablet on to collect signature
+            sigPlusNET1.SetTranslateBitmapEnable(false);
+
+            //Images sent to the background
+            sigPlusNET1.LCDSendGraphic(1, 2, -2, 0, sign);
+            sigPlusNET1.LCDSendGraphic(1, 2, 199, 48, done);
+            sigPlusNET1.LCDSendGraphic(1, 2, 196, 5, clear);
+
+            sigPlusNET1.SetDisplayPenWidth(10);
+
+            sigPlusNET1.ClearTablet();
+            sigPlusNET1.ClearSigWindow(1);
+            sigPlusNET1.LCDRefresh(2, 0, 0, 240, 64); //Brings the background image already loaded into foreground
+            sigPlusNET1.KeyPadAddHotSpot(2, 1, 191, 0, 55, 22); //For CLEAR button
+            sigPlusNET1.KeyPadAddHotSpot(3, 1, 194, 45, 48, 22); //For DONE button
+            sigPlusNET1.LCDSetWindow(16, 16, 174, 44);
+            sigPlusNET1.SetSigWindow(1, 16, 16, 174, 44); //Sets the area where ink is permitted in the SigPlus object
+            sigPlusNET1.SetLCDCaptureMode(2);   //Sets mode so ink will not disappear after a few seconds
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //reset hardware
+            sigPlusNET1.LCDRefresh(0, 0, 0, 240, 64);
+            sigPlusNET1.LCDSetWindow(0, 0, 240, 64);
+            sigPlusNET1.SetSigWindow(1, 0, 0, 240, 64);
+            sigPlusNET1.KeyPadClearHotSpotList();
+            // Bitmap blank = new System.Drawing.Bitmap(240, 64);
+            // sigPlusNET1.LCDSendGraphic(1, 0, 0, 0, blank);
+            sigPlusNET1.SetLCDCaptureMode(1);
+            sigPlusNET1.SetTabletState(0);
+        }
+
+        private void sigPlusNET1_PenUp(object sender, EventArgs e)
+        {
+            if (sigPlusNET1.KeyPadQueryHotSpot(2) > 0) //If the CLEAR hotspot is tapped, then...
+            {
+                sigPlusNET1.ClearSigWindow(1);
+                sigPlusNET1.ClearTablet();
+                sigPlusNET1.LCDRefresh(1, 16, 16, 175, 44); //Refresh LCD at 'CLEAR' to indicate to user that this option has been sucessfully chosen
+                sigPlusNET1.LCDRefresh(2, 0, 0, 240, 64); //Brings the background image already loaded into foreground
+            }
+            else if (sigPlusNET1.KeyPadQueryHotSpot(3) > 0) //If the DONE hotspot is tapped, then...
+            {
+                sigPlusNET1.ClearSigWindow(1);
+
+                if (sigPlusNET1.NumberOfTabletPoints() > 0)
+                {
+                    saveImage();
+                }
+                else
+                {
+                    sigPlusNET1.LCDRefresh(0, 0, 0, 240, 64);
+                    sigPlusNET1.LCDSendGraphic(0, 2, 4, 20, please);
+                    System.Threading.Thread.Sleep(750);
+                    sigPlusNET1.ClearTablet();
+                    sigPlusNET1.LCDRefresh(2, 0, 0, 240, 64);
+                    sigPlusNET1.SetLCDCaptureMode(2);   //Sets mode so ink will not disappear after a few seconds
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            sigPlusNET1.ClearSigWindow(1);
+            sigPlusNET1.ClearTablet();
+            sigPlusNET1.LCDRefresh(1, 16, 16, 175, 44); //Refresh LCD at 'CLEAR' to indicate to user that this option has been sucessfully chosen
+            sigPlusNET1.LCDRefresh(2, 0, 0, 240, 64); //Brings the background image already loaded into foreground
+        }
+
+        private void doneButton_Click(object sender, EventArgs e)
+        {
+            sigPlusNET1.ClearSigWindow(1);
+            
+            // If there are tablet points, call saveImage; otherwise, show a message on the LCD.
+            if (sigPlusNET1.NumberOfTabletPoints() > 0)
+            {
+                saveImage();
+            }
+            else
+            {
+                MessageBox.Show("No signature captured. Please sign on the tablet before clicking Done.", 
+                        "No Signature Detected", 
+                        MessageBoxButtons.OK, 
+                        MessageBoxIcon.Warning);
+            }
+        }
+
+        private void saveImage()
+        {
+            sigPlusNET1.SetImageXSize(2000);
+            sigPlusNET1.SetImageYSize(700);
+            sigPlusNET1.SetJustifyY(10);
+            sigPlusNET1.SetJustifyX(10);
+            sigPlusNET1.SetJustifyMode(5);
+            sigPlusNET1.SetImagePenWidth(86);
+            sigPlusNET1.SetImageFileFormat(4);
+            Image sigImage = sigPlusNET1.GetSigImage();
+            string filePath = Path.Combine(Application.StartupPath, "signature.png");
+            sigImage.Save(filePath, ImageFormat.Png);
+            sigPlusNET1.LCDRefresh(0, 0, 0, 240, 64);
+            Font f = new("Arial", 9.0F, FontStyle.Regular);
+            sigPlusNET1.LCDWriteString(0, 2, 35, 25, f, "Signature capture complete.");
+            System.Threading.Thread.Sleep(1500);
+            Application.Exit();
+        }
+    }
+}
